db4cd407a05075b0557cc6e529926b2ef4caa191
dna <diana.thoas@gmail.com>
2026-05-13 23:16:40 +0200

    handle wrong helper address on start up

--- start_up.sh
+++ start_up.sh
@@ -10,6 +10,12 @@ else
 fi
 # get helper server
 helper_server="${helper#*@}"
+# check if helper is reachable
+if ! nc -z -w 5 "$helper_server" 22 2>/dev/null; then
+    echo "helper unreachable..."
+    rm -f /root/helper
+    exit 1
+fi
 # get unknown host keys in silence and add to known_hosts
 mkdir -p ~/.ssh
 ssh-keyscan "$helper_server" >> ~/.ssh/known_hosts 2>/dev/null
